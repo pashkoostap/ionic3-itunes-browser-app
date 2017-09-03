8891909039c31b3859046a913f4f7621a12aa151
pashko.ostap <pashko.ostap@gmail.com>
2017-09-03 23:35:20 +0300

    implemented ionic dialogs

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -3,7 +3,8 @@ import {
   NavController,
   NavParams,
   ActionSheetController,
-  ModalController
+  ModalController,
+  AlertController
 } from 'ionic-angular';
 import { PreviewPage } from '../preview/preview';
 import { ItunesProvider } from '../../providers/itunes/itunes';
@@ -14,7 +15,7 @@ import { ItunesProvider } from '../../providers/itunes/itunes';
 })
 export class SearchPage {
   results: Array<any> = [];
-  keyword: String;
+  keyword: String = '';
   usersFilter: boolean;
   unfilteredResults: Array<any> = [];
 
@@ -23,23 +24,66 @@ export class SearchPage {
     public navParams: NavParams,
     private itunes: ItunesProvider,
     private actionSheetCtrl: ActionSheetController,
-    public modalCtrl: ModalController
+    public modalCtrl: ModalController,
+    public alertCtrl: AlertController
   ) {}
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SearchPage');
-  }
 
   getIconType(result) {
     return result.kind === 'song' ? 'musical-notes' : 'film';
+  }
+
+  search() {
+    this.itunes.search(this.keyword).then(results => {
+      if (!results.length) {
+        const alert = this.alertCtrl.create({
+          title: 'The iTunes API says...',
+          subTitle: 'No match found',
+          buttons: ['Try again']
+        });
+        alert.present();
+      }
+
+      this.results = results;
+      this.unfilteredResults = results;
+      this.usersFilter = false;
+    });
   }
 
   onSearch(e) {
     const { keyCode } = e;
     const searchValue = this.keyword.toLocaleLowerCase();
 
+    if (this.keyword.trim() === '') {
+      const alert = this.alertCtrl.create({
+        title: 'Empty search not allowed',
+        subTitle: 'Please key in your search',
+        buttons: [
+          'Cancel',
+          {
+            text: 'Search...',
+            handler: data => {
+              if (data.term) {
+                this.keyword = data.term;
+                this.search();
+              } else {
+                return false;
+              }
+            }
+          }
+        ],
+        inputs: [
+          {
+            name: 'term',
+            placeholder: 'Search for...'
+          }
+        ]
+      });
+
+      return alert.present();
+    }
+
     if (keyCode !== 13) {
-      this.itunes.search(searchValue).then(results => (this.results = results));
+      this.search();
     }
   }
 
@@ -95,11 +139,22 @@ export class SearchPage {
   }
 
   openPreview(track) {
-    const modal = this.modalCtrl.create(PreviewPage, {
-      track
+    const alert = this.alertCtrl.create({
+      title: 'Show preview?',
+      buttons: [
+        'No',
+        {
+          text: 'Yes!',
+          handler: () => {
+            const modal = this.modalCtrl.create(PreviewPage, {
+              track
+            });
+            modal.present();
+          }
+        }
+      ]
     });
-
-    modal.present();
+    alert.present();
   }
 
   reloadData(refresher) {
